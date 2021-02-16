0efb31fbc17263067a4de79aaf7c3c4b86d3dbb4
Syed <sh13226@my.bristol.ac.uk>
2021-02-16 18:31:33 +0000

    gdt.cpp : renamed i to GDTDescriptor

--- gdt.cpp
+++ gdt.cpp
@@ -11,10 +11,10 @@ GlobalDescriptorTable::GlobalDescriptorTable()
       codeSegmentSelector(0, 64*1024*1024, 0x9A),
       dataSegmentSelector(0, 64*1024*1024, 0x92)
 {
-    uint32_t i[2];
-    i[1] = (uint32_t)this;
-    i[0] = sizeof(GlobalDescriptorTable) << 16;
-    asm volatile("lgdt (%0)" : : "p"(((uint8_t *)i)+2));
+    uint32_t GDTDescriptor[2];
+    GDTDescriptor[1] = (uint32_t)this;
+    GDTDescriptor[0] = sizeof(GlobalDescriptorTable) << 16;
+    asm volatile("lgdt (%0)" : : "p"(((uint8_t *)GDTDescriptor)+2));
 }
 
 // Destructor
