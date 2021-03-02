bb04aec1e3a591c0277f3ee339e952905ce108d3
Syed <sh13226@my.bristol.ac.uk>
2021-03-02 21:10:25 +0000

    Added gui::Widget class members

--- include/gui/widget.h
+++ include/gui/widget.h
@@ -5,8 +5,31 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include "common/types.h"
+
 namespace morphios {
 namespace gui {
+
+class Widget {
+protected:
+    Widget *parent; // Parent widget
+
+    // Positional members
+    common::int32_t x;
+    common::int32_t y;
+    common::int32_t w; // Width
+    common::int32_t h; // Height
+
+    // Colour related members
+    common::uint8_t r;
+    common::uint8_t g;
+    common::uint8_t b;
+
+public:
+    Widget(common::int32_t x, common::int32_t y, common::int32_t w, common::int32_t h);
+    ~Widget();
+    void draw();
+};
     
 } // namespace gui    
 } // namespace morphios
