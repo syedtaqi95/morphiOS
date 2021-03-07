f0181124640327308e1729ad33a4ee04d657095e
Syed <sh13226@my.bristol.ac.uk>
2021-03-07 11:46:37 +0000

    Added MemoryManager files

---
+++ include/kernel/memorymanager.h
@@ -0,0 +1,13 @@
+/*
+    memorymanagement.h - MMU class header file
+*/
+
+namespace morphios {
+namespace kernel {
+
+class MemoryManager {
+
+};
+
+} // namespace kernel
+} // namespace morphios
---
+++ src/kernel/memorymanager.cpp
@@ -0,0 +1,8 @@
+/*
+    memorymanagement.cpp - MMU class implementation
+*/
+
+#include "kernel/memorymanager.h"
+
+using namespace morphios::common;
+using namespace morphios::kernel;
